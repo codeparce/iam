5bf8fdbb31ffd14175e7c1f33b7d0ba2bacce7e8
code parce <codeparcelts@gmail.com>
2026-02-23 16:00:40 -0500

    DEVOPS : NEW : Deploy en github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+
+// ⚠️ Cambia 'repositorio' por el nombre real de tu repo
+const repoName = '/iam/'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: repoName,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
